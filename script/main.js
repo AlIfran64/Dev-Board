600562c90e2b754fb91a5ace934fb241f4281ecc
Al Ifran <ifran15-2915@diu.edu.bd>
2025-03-01 17:43:12 +0600

    left part done

---
+++ script/main.js
@@ -0,0 +1,29 @@
+// Box-3-date start
+let box3 = document.getElementById("box-3-date");
+
+let date = new Date();
+let parts = date.toDateString().split(" ");
+let formattedDate = `${parts[1]} ${parts[2]} ${parts[3]}`;
+
+let dayElement = document.createElement("p");
+dayElement.innerText = parts[0] + ",";
+dayElement.style.fontSize = "20px";
+
+let dateElement = document.createElement("p");
+dateElement.innerHTML = `<strong>${formattedDate}</strong>`;
+dateElement.style.fontSize = "20px";
+
+box3.appendChild(dayElement);
+box3.appendChild(dateElement);
+
+// Box-3-date end
+
+// Deadline date start
+document.getElementById("deadline");
+deadline("deadline1");
+deadline("deadline2");
+deadline("deadline3");
+deadline("deadline4");
+deadline("deadline5");
+deadline("deadline6");
+// Deadline date end
--- utilities/utilities.js
+++ utilities/utilities.js
@@ -5,3 +5,13 @@ function changeBodyColor() {
   let b = Math.floor(Math.random() * 256);
   return `rgb(${r}, ${g}, ${b})`;
 }
+
+// Deadline:
+function deadline(id) {
+  let deadlineDate = new Date();
+  let currentDeadline = deadlineDate.toDateString().split(" ");
+  let formattedDeadline = ` ${currentDeadline[2]} ${currentDeadline[1]} ${currentDeadline[3]}`;
+  let deadline = document.createElement("p");
+  deadline.innerHTML = `<strong>${formattedDeadline}</strong>`;
+  document.getElementById(id).appendChild(deadline);
+}
